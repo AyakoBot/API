ebccb0bce278c0fc6009b3478216867fc6437106
Larsundso <Larsundso3@gmail.com>
2026-03-05 22:50:11 +0100

    refactor: Use correct imports

--- src/API.ts
+++ src/API.ts
@@ -1,27 +1,25 @@
 import type { Cache, logger as Logger } from '@ayako/utility';
-import {
- ApplicationCommandsAPI,
- ApplicationsAPI,
- ChannelsAPI,
- API as DiscordAPI,
- GatewayAPI,
- GuildsAPI,
- InteractionsAPI,
- InvitesAPI,
- MonetizationAPI,
- OAuth2API,
- PollAPI,
- RoleConnectionsAPI,
- SoundboardSoundsAPI,
- StageInstancesAPI,
- StickersAPI,
- ThreadsAPI,
- UsersAPI,
- VoiceAPI,
- WebhooksAPI,
-} from '@discordjs/core';
+import { GatewayAPI, API as DiscordAPI } from '@discordjs/core';
 import { REST } from '@discordjs/rest';
 import { EventEmitter } from 'events';
+
+import ApplicationCommandsAPI from './APITypes/ApplicationCommandsAPI.js';
+import ApplicationsAPI from './APITypes/ApplicationsAPI.js';
+import ChannelsAPI from './APITypes/ChannelsAPI.js';
+import GuildsAPI from './APITypes/GuildsAPI.js';
+import InteractionsAPI from './APITypes/InteractionsAPI.js';
+import InvitesAPI from './APITypes/InvitesAPI.js';
+import MonetizationAPI from './APITypes/MonetizationAPI.js';
+import OAuth2API from './APITypes/OAuth2API.js';
+import PollAPI from './APITypes/PollAPI.js';
+import RoleConnectionsAPI from './APITypes/RoleConnectionsAPI.js';
+import SoundboardSoundsAPI from './APITypes/SoundboardSoundsAPI.js';
+import StageInstancesAPI from './APITypes/StageInstancesAPI.js';
+import StickersAPI from './APITypes/StickersAPI.js';
+import ThreadsAPI from './APITypes/ThreadsAPI.js';
+import UsersAPI from './APITypes/UsersAPI.js';
+import VoiceAPI from './APITypes/VoiceAPI.js';
+import WebhooksAPI from './APITypes/WebhooksAPI.js';
 
 export default class API extends EventEmitter {
  logger: typeof Logger;
@@ -50,7 +48,7 @@ export default class API extends EventEmitter {
  rest: REST;
  botId: string;
 
- constructor(token: string, logger: typeof Logger, cache: Cache) {
+ constructor(token: string, logger: typeof Logger, cache: Cache, guildId: string) {
   super();
   this.logger = logger;
   this.cache = cache;
@@ -61,23 +59,23 @@ export default class API extends EventEmitter {
   this.api = new DiscordAPI(this.rest);
   this.botId = new Buffer(token.replace('Bot ', '').split('.')[0], 'base64').toString();
 
-  this.applicationCommands = new ApplicationCommandsAPI(this.rest);
-  this.applications = new ApplicationsAPI(this.rest);
-  this.channels = new ChannelsAPI(this.rest);
+  this.applicationCommands = new ApplicationCommandsAPI(token, this.logger, guildId);
+  this.applications = new ApplicationsAPI(token, this.logger, guildId);
+  this.channels = new ChannelsAPI(token, this.logger, guildId, this.cache);
   this.gateway = new GatewayAPI(this.rest);
-  this.guilds = new GuildsAPI(this.rest);
-  this.invites = new InvitesAPI(this.rest);
-  this.monetization = new MonetizationAPI(this.rest);
-  this.oauth2 = new OAuth2API(this.rest);
-  this.poll = new PollAPI(this.rest);
-  this.roleConnections = new RoleConnectionsAPI(this.rest);
-  this.soundboardSounds = new SoundboardSoundsAPI(this.rest);
-  this.stageInstances = new StageInstancesAPI(this.rest);
-  this.stickers = new StickersAPI(this.rest);
-  this.threads = new ThreadsAPI(this.rest);
-  this.users = new UsersAPI(this.rest);
-  this.voice = new VoiceAPI(this.rest);
-  this.webhooks = new WebhooksAPI(this.rest);
-  this.interactions = new InteractionsAPI(this.rest, this.webhooks);
+  this.guilds = new GuildsAPI(token, this.logger, guildId, this.cache);
+  this.invites = new InvitesAPI(token, this.logger, guildId, this.cache);
+  this.monetization = new MonetizationAPI(token, this.logger, guildId);
+  this.oauth2 = new OAuth2API(token, this.logger, guildId);
+  this.poll = new PollAPI(token, this.logger, guildId, this.cache);
+  this.roleConnections = new RoleConnectionsAPI(token, this.logger, guildId);
+  this.soundboardSounds = new SoundboardSoundsAPI(token, this.logger, guildId);
+  this.stageInstances = new StageInstancesAPI(token, this.logger, guildId, this.cache);
+  this.stickers = new StickersAPI(token, this.logger, guildId);
+  this.threads = new ThreadsAPI(token, this.logger, guildId, this.cache);
+  this.users = new UsersAPI(token, this.logger, guildId);
+  this.voice = new VoiceAPI(token, this.logger, guildId, this.cache);
+  this.webhooks = new WebhooksAPI(token, this.logger, guildId);
+  this.interactions = new InteractionsAPI(token, this.logger, guildId);
  }
 }
